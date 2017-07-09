9bbaf326f2da1dad97ad61b117b4d7a41c0e9592
vaishnavi789 <vaishnavi.raju@gmail.com>
2017-07-09 10:37:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -211,7 +211,35 @@ getAllQuestion().then(function(returnVal){
                     } else if (req.body.result.parameters.yesno.length != 0) { //if param value is ues or no
                         monitorAnswers.push(req.body.result.parameters.yesno);  //pushing into monitor answers
                     }
-                    mCount = 0; //Setting mCount back to zero
+
+                                      if (mCount >= monitoring.length){
+                                       if(hCount == 1){
+                                       text = high[hCount].title;
+                                       if (req.body.result.parameters.number.length != 0) { //valid number
+                                             highAnswers.push(req.body.result.parameters.number); //storing number parameter value into monitor answers
+                                      }else if (req.body.result.parameters.yesno.length != 0) { //if param value is ues or no
+                                             highAnswers.push(req.body.result.parameters.yesno);  //pushing into monitor answers
+                                            }
+                                           hCount ++;
+                                           }
+                                           console.log(highAnswers);
+                                       }
+
+                                       if (mCount >= monitoring.length){
+                                        if(lCount == 1){
+                                        text = low[lCount].title;
+                                      if (req.body.result.parameters.number.length != 0) { //valid number
+                                          lowAnswers.push(req.body.result.parameters.number); //storing number parameter value into monitor answers
+                                      }else if (req.body.result.parameters.yesno.length != 0) { //if param value is ues or no
+                                         lowAnswers.push(req.body.result.parameters.yesno);  //pushing into monitor answers
+                                         }
+                                        lCount ++;
+                                         }
+                                         console.log(lowAnswers);
+                                       }
+
+                                      mCount = 0;
+
 
                     var ate = monitorAnswers[0];     //Storing answers at the end
                     var sugarLevel = monitorAnswers[1];
@@ -256,30 +284,6 @@ getAllQuestion().then(function(returnVal){
             }
                  mCount ++;
 
-
-                   if(hCount == 1){
-                   text = high[hCount].title;
-                   if (req.body.result.parameters.number.length != 0) { //valid number
-                         highAnswers.push(req.body.result.parameters.number); //storing number parameter value into monitor answers
-                  }else if (req.body.result.parameters.yesno.length != 0) { //if param value is ues or no
-                         highAnswers.push(req.body.result.parameters.yesno);  //pushing into monitor answers
-                        }
-                     hCount ++;
-                       }
-                    console.log(highAnswers);
-
-
-
-                   if(lCount == 1){
-                   text = low[lCount].title;
-                  if (req.body.result.parameters.number.length != 0) { //valid number
-                      lowAnswers.push(req.body.result.parameters.number); //storing number parameter value into monitor answers
-                  }else if (req.body.result.parameters.yesno.length != 0) { //if param value is ues or no
-                     lowAnswers.push(req.body.result.parameters.yesno);  //pushing into monitor answers
-                     }
-                    lCount ++;
-                     }
-                 console.log(lowAnswers);
 
            break;
 
