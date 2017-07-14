b5605d68e11d0f84d83a2c9941f707113fc73e79
vaishnavi789 <vaishnavi.raju@gmail.com>
2017-07-14 14:09:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -241,38 +241,35 @@ getAllQuestion().then(function(returnVal){
             hCount = 1;
             }else if(ate = "no" && sugarLevel > 7){
             hCount = 1;
-           }
+            }
              hCount ++;
-             
-            //  if (req.body.result.parameters.number.length != 0) {
-            //     monitorAnswers.push(req.body.result.parameters.number);
-            // } else if (req.body.result.parameters.yesno.length != 0) {
-            //     monitorAnswers.push(req.body.result.parameters.yesno);
-            // }
-                   
-            //
-            //           var ate = monitorAnswers[0];     //Storing answers at the end
-            //           var sugarLevel = monitorAnswers[1];
-            //           var medication = monitorAnswers[2];
-            //           var exercise = monitorAnswers[3];
-            //           var weight = monitorAnswers[4];
-            //
-            //           console.log(monitorAnswers);
-
             break;
       }
 
             if  (hCount >= high.length){
 
-                // mCount = 0;
-                 hCount = 0;
+                 if (req.body.result.parameters.number.length != 0) {
+                    monitorAnswers.push(req.body.result.parameters.number);
+                } else if (req.body.result.parameters.yesno.length != 0) {
+                   monitorAnswers.push(req.body.result.parameters.yesno);
+                }
 
+                mCount = 0;
+                hCount = 0;
+
+          var ate = monitorAnswers[0];     //Storing answers at the end
+          var sugarLevel = monitorAnswers[1];
+          var medication = monitorAnswers[2];
+          var exercise = monitorAnswers[3];
+          var weight = monitorAnswers[4];
+
+          console.log(monitorAnswers);
            date = req.body.timestamp;
            console.log(date);
 
-      //     text = "I'll get this logged for you ASAP. "
-          //     + monitorResult(ate, sugarLevel, exercise, weight);
-           //+ "What else can I do for you?";
+          text = "I'll get this logged for you ASAP. "
+               + monitorResult(ate, sugarLevel, exercise, weight);
+               + "What else can I do for you?";
            break;
       }
 
