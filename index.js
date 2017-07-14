a532b2aa94ecc2e77631edc6888c32d27cbca727
vaishnavi789 <vaishnavi.raju@gmail.com>
2017-07-14 10:35:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -227,6 +227,22 @@ getAllQuestion().then(function(returnVal){
             case "start.monitor":
           if (mCount >= monitoring.length){
 
+            if (req.body.result.parameters.number.length != 0) {
+               monitorAnswers.push(req.body.result.parameters.number);
+           } else if (req.body.result.parameters.yesno.length != 0) {
+               monitorAnswers.push(req.body.result.parameters.yesno);
+           }
+                     mCount = 0;
+
+                     var ate = monitorAnswers[0];     //Storing answers at the end
+                     var sugarLevel = monitorAnswers[1];
+                     var medication = monitorAnswers[2];
+                     var exercise = monitorAnswers[3];
+                     var weight = monitorAnswers[4];
+
+                     console.log(monitorAnswers);
+
+
            text = high[hCount].title;
 
            if (req.body.result.parameters.number.length != 0) {
@@ -242,29 +258,13 @@ getAllQuestion().then(function(returnVal){
             }else if(ate = "no" && sugarLevel > 7){
             hCount = 1;
            }
-
              hCount ++;
              break;
       }
 
             if  (hCount >= high.length){
 
-                   hCount = 0;
-
-              if (req.body.result.parameters.number.length != 0) {
-                 monitorAnswers.push(req.body.result.parameters.number);
-             } else if (req.body.result.parameters.yesno.length != 0) {
-                 monitorAnswers.push(req.body.result.parameters.yesno);
-             }
-                       mCount = 0;
-
-                       var ate = monitorAnswers[0];     //Storing answers at the end
-                       var sugarLevel = monitorAnswers[1];
-                       var medication = monitorAnswers[2];
-                       var exercise = monitorAnswers[3];
-                       var weight = monitorAnswers[4];
-
-                          console.log(monitorAnswers);
+                 hCount = 0;
 
            date = req.body.timestamp;
            console.log(date);
