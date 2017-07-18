5af189d5eface5ab1b404b99e4ff6ff2ff427a30
vaishnavi789 <vaishnavi.raju@gmail.com>
2017-07-17 20:08:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -250,18 +250,17 @@ getAllQuestion().then(function(returnVal){
                 }else{
                   cool = 0;
                 }             
-                   
+
+                if (cool == 0){
+                  
+                text = "I'll get this logged for you ASAP. "
+                    + monitorResult(ate, sugarLevel, exercise, weight);
+                     + "What else can I do for you?";                                 
+                     break;
+             }     
+                                                
        break;
     }
-
-       if (cool == 0){
-         
-       text = "I'll get this logged for you ASAP. "
-           + monitorResult(ate, sugarLevel, exercise, weight);
-            + "What else can I do for you?";     
-                   
-            break;
-      }
 
            if  (cool == 1){
            text = high[hCount].title;
