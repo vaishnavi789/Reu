7b2e2f9f6b462bb9c5ba4c8f8c5c4c2e7f9fc0fc
vaishnavi789 <vaishnavi.raju@gmail.com>
2017-07-20 10:17:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -271,7 +271,7 @@ getAllQuestion().then(function(returnVal){
                         break;
                 }
 
-                if (lCount >= low.length){
+                 else if (lCount >= low.length){
                     if (req.body.result.parameters.number.length != 0) {
                         lowAnswers.push(req.body.result.parameters.number);
                     } else if (req.body.result.parameters.yesno.length != 0) {
@@ -353,18 +353,15 @@ getAllQuestion().then(function(returnVal){
                     break;
                }
 
-               if (cold == 1){
+                 if (cold == 1){
                    text = low[lCount].title;
 
                    if (req.body.result.parameters.number.length != 0) {
                        lowAnswers.push(req.body.result.parameters.number);
                    } else if (req.body.result.parameters.yesno.length != 0) {
                        lowAnswers.push(req.body.result.parameters.yesno);
-                   }
-
-                   if (lCount == 1){
-                    lCount ++;
-                 }
+                   }                
+                    lCount ++;                 
                     break;
               }
 
